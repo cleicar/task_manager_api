65a40ccce2ad9e2278deb634329e138aa8c5b056
Cleiviane Costa <costa.cleiviane@gmail.com>
2017-08-21 17:17:09 -0300

    Adding api version constraint to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   # Integração com a Escrita Fiscal
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api'}, path: '/' do
-
+  	namespace :v1 do
+  		resources :tasks
+  	end
   end
 
 end
