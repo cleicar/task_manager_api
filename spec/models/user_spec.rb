6f178344a1131534091ff11dea6798780cdb6cb1
Cleiviane Costa <costa.cleiviane@gmail.com>
2017-08-22 19:07:18 -0300

    User generate authentication method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe User, type: :model do
 
 	let(:user) { build(:user) }
 
-  describe 'Testing user fields' do
-  	it { is_expected.to be_mongoid_document }
+	describe 'Testing user fields' do
+		it { is_expected.to be_mongoid_document }
 
 		it { is_expected.to have_fields(:email).of_type(String) }
 
@@ -26,6 +26,23 @@ RSpec.describe User, type: :model do
 				allow(Devise).to receive(:friendly_token).and_return('abu71878soTOKEN')
 
 				expect(user.info).to eq "#{user.email} - #{user.created_at} - Token: abu71878soTOKEN"
+			end
+		end
+
+		describe '#generate_authentication_token!' do
+			it 'generates a unique auth token' do
+				allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
+				user.generate_authentication_token!
+
+				expect(user.auth_token).to eq('abc123xyzTOKEN')
+			end
+
+			it 'generates another auth token when the current auth token already has been taken' do
+				allow(Devise).to receive(:friendly_token).and_return('abc123tokenxyz', 'abc123tokenxyz', 'abcXYZ123456789')
+				existing_user = create(:user)
+				user.generate_authentication_token!
+
+				expect(user.auth_token).not_to eq(existing_user.auth_token)
 			end
 		end
 	end
