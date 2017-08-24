9348aaf58edf25be0062e7636e1e83ce8db75d47
Cleiviane Costa <costa.cleiviane@gmail.com>
2017-08-23 23:37:48 -0300

    Metodo para pegar o current_user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
 	before_action :set_user, only: [:update, :show, :destroy]
+	#before_action :authenticate_user!, only: [:update, :destroy]
 
 	respond_to :json
 
---
+++ app/controllers/concerns/authentication.rb
@@ -0,0 +1,10 @@
+module Authentication
+
+	def current_user
+		@current_user || User.find_by(auth_token: request.headers['Authorization'])
+	end
+
+	def authenticate_user!
+
+	end
+end
